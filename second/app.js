9dbbec3a49fbff788b59ef0c3262bc8efc74687a
KwinZainy <gbadamosiblessing651@gmail.com>
2025-05-09 19:16:56 +0100

    commented the js

--- second/app.js
+++ second/app.js
@@ -3,24 +3,24 @@ const submit = document.getElementById('submit')
 const counter = document.getElementById('counter')
 const colorChange = document.getElementById("colorChange")
 
-// function textLength(){
-//     if(textarea.value.length >= 20){
-//         alert('too much words')
-//     }
+function textLength(){
+    if(textarea.value.length >= 20){
+        alert('too much words')
+    }
 
 
-// }
-
-function onChange(){
-        // counter
-        let count = textarea.value.length
-        counter.innerText = count
-
-        if(textarea.value.length >= 250 && counter.innerText >= 250){
-            colorChange.style.color = "red"
-            counter.style.color = "red"
-        }
 }
+
+// function onChange(){
+//         // counter
+//         let count = textarea.value.length
+//         counter.innerText = count
+
+//         if(textarea.value.length >= 250 && counter.innerText >= 250){
+//             colorChange.style.color = "red"
+//             counter.style.color = "red"
+//         }
+// }
 
 
 
